a4ca723cb7407eb1fbbabbdbcd257bcca2e7c3b0
PabloAvelar <pabloarmenta84@gmail.com>
2024-03-09 19:12:17 -0600

    page, table, modal and data finished

---
+++ src/components/PopupSuppliers.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmarkSquare } from '@fortawesome/free-solid-svg-icons'
+import '../styles/popupclients.css';
+import clientsService from '../services/clientsService';
+
+function PopupSuppliers({ closeModal, data }) {
+    const [inputs, setInputs] = useState({});
+
+    useEffect(() => {
+        // Setting data to edit a customer
+        if (data) {
+            setInputs(values => ({ ...values, ['suplier_id']: data.suplier_id }))
+
+            document.clientForm.suplier_name.value = data.suplier_name;
+            setInputs(values => ({ ...values, ['suplier_name']: data.suplier_name }))
+
+            document.clientForm.contact_person.value = data.contact_person;
+            setInputs(values => ({ ...values, ['contact_person']: data.contact_person }))
+
+            document.clientForm.suplier_contact.value = data.suplier_contact;
+            setInputs(values => ({ ...values, ['suplier_contact']: data.suplier_contact }))
+
+            document.clientForm.suplier_address.value = data.suplier_address;
+            setInputs(values => ({ ...values, ['suplier_address']: data.suplier_address }))
+
+            document.clientForm.note.value = data.note;
+            setInputs(values => ({ ...values, ['note']: data.note }))
+
+        }
+    }, [])
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setInputs(values => ({ ...values, [name]: value }));
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+
+
+            const data = new URLSearchParams({
+                'suplier_id': inputs.suplier_id,
+                'suplier_name': inputs.suplier_name,
+                'contact_person': inputs.contact_person,
+                'suplier_contact': inputs.suplier_contact,
+                'suplier_address': inputs.suplier_address,
+                'note': inputs.note,
+            })
+
+            // If it's a new customer
+            if (inputs.suplier_id === undefined) {
+                data.delete("suplier_id");
+
+                clientsService.postClient(data.toString())
+                    .then((res) => {
+                        if (res.status === 'success') {
+                            console.log("cliente agregado");
+                            window.location.reload();
+                        }
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                    })
+            } else {
+                // If a client is being edited
+                clientsService.putClient(data.toString())
+                    .then((res) => {
+                        if (res.status === 'success') {
+                            console.log("cliente editado");
+                            window.location.reload();
+                        }
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                    })
+            }
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    return (
+
+        <div className='popup-client-card-container shadow'>
+            <a className='close-modal' onClick={() => closeModal(false)}>
+                <FontAwesomeIcon icon={faXmarkSquare} size='2x' color='#260B01' />
+            </a>
+            <div style={{ marginTop: 20 }}>
+                <span style={{ color: "#370021", fontWeight: 'bold', fontSize: 36 }}>
+                    Supplier Information
+                </span>
+            </div>
+            <form className='popup-client-card' onSubmit={handleSubmit} name='clientForm'>
+
+                {/* <br> */}
+
+                <div className='input-container'>
+                    <div className="input-add-client-container">
+                        <span style={{ fontSize: 16 }}>Full Name: </span>
+                        <input className='input-form-popup' onChange={handleChange} type="text" name="suplier_name" required />
+                    </div>
+                    <div className="input-add-client-container">
+                        <span style={{ fontSize: 16 }}>Phone Number: </span>
+                        <input className='input-form-popup' onChange={handleChange} type="text" name="contact_person" required />
+                    </div>
+                    <div className="input-add-client-container">
+                        <span style={{ fontSize: 16 }}>suplier_contact: </span>
+                        <input className='input-form-popup' onChange={handleChange} type="text" name="suplier_contact" required />
+                    </div>
+                    <div className="input-add-client-container">
+                        <span style={{ fontSize: 16 }}>Membership #: </span>
+                        <input className='input-form-popup' onChange={handleChange} type="text" name="suplier_address" required />
+                    </div>
+                    <div className="input-add-client-container">
+                        <span style={{ fontSize: 16 }}>Note: </span>
+                        <input className='input-form-popup' onChange={handleChange} type="text" name="note" required />
+                    </div>
+                </div>
+
+                <div className="submit-container">
+                    <button className="submit-button shadow" type="submit">Save</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default PopupSuppliers;
--- src/pages/Suppliers.js
+++ src/pages/Suppliers.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import TableSuppliers from '../components/TableSuppliers';
+import '../styles/clients.css';
+import PopupClients from '../components/PopupClients';
+import suppliersService from '../services/suppliersService';
 
 function Suppliers() {
+
+  const [suppliers, setSuppliers] = useState([]);
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    // Cargando sólo una vez los clientes que hay
+    suppliersService.getSuppliers()
+      .then((clients) => {
+        setSuppliers(clients);
+        setDataLoaded(true);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }, []);
+
+  console.log(suppliers);
   return (
-    <div>Suppliers</div>
-  )
+
+    <main className="page-container">
+      <section className='clients-container'>
+        <div className='page-title-container'>
+          <span className='page-title'>Suppliers</span>
+        </div>
+        <div className='add-customer-content'>
+          <div className='add-customer-container'>
+            <a className='add-customer' onClick={() => {
+              setShowModal(true);
+            }}>+ Add Supplier</a>
+          </div>
+        </div>
+      </section>
+
+      {dataLoaded ? <TableSuppliers data={suppliers} /> : <p>cargando datos</p>}
+      {showModal && <PopupClients closeModal={setShowModal} />}
+
+    </main>
+  );
 }
 
 export default Suppliers
