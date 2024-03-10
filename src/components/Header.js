f56323a0bf1853bf90b9ca7fae1a3cf1516bc1dd
PabloAvelar <pabloarmenta84@gmail.com>
2024-03-10 01:47:00 -0600

    fix sidebar style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import logo from '../images/logo.png';
 import '../styles/header.css';
+import { useAuth } from './AuthContext';
 
 function Header() {
+    const auth = useAuth();
     const getCurrentDateTime = () => {
         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
         const currentDateTime = new Date().toLocaleString(undefined, options);
         return currentDateTime;
     };
-
+    if (!auth.auth){
+        // porque esta mamada tarda en cargar por las promesas miadas
+        return <></>
+    }
     return (
         <div className="header-container">
             <div>
               
-                <div className="welcome-message">Bienvenido: User</div>
+                <div className="welcome-message">Bienvenido: {auth.auth.username}</div>
             </div>
             <div className="date-time">
                 <div className="time">{getCurrentDateTime().split(',')[1]}</div>
