ecdc7e3e8e28bda0235ca17ced8795649f99d6c9
PabloAvelar <pabloarmenta84@gmail.com>
2024-03-10 14:21:25 -0600

    fix page container

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,37 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../components/AuthContext';
+import React, { useState, useEffect } from 'react'
+import TableSuppliers from '../components/TableSuppliers';
+import '../styles/clients.css';
+import PopupSuppliers from '../components/PopupSuppliers';
+import suppliersService from '../services/suppliersService';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
-import '../styles/sidebar.css';
 
-import '../styles/header.css';
 function Dashboard() {
-  let navigate = useNavigate();
-  const auth = useAuth();
 
-  const handleLogout = () => {
-    auth.logout();
-    navigate("/");
-  }
+  const [suppliers, setSuppliers] = useState([]);
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-  if (!auth.auth){
-    return <></>
-  }
+  useEffect(() => {
+    // Cargando sólo una vez los clientes que hay
+    suppliersService.getSuppliers()
+      .then((clients) => {
+        setSuppliers(clients);
+        setDataLoaded(true);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }, []);
+
+  console.log(suppliers);
   return (
-    
-    <div>
-    
-     <Header/> 
-    <Sidebar>
-    {/* <p>Dashboard</p>
-      <p>PINCHES VIEJAS PUTAS {auth.auth.username}</p>
-       */}
-      
-      </Sidebar>
-    </div>
 
-  )
+    <main className="page-container">
+
+      <aside>
+        <Sidebar />
+      </aside>
+
+      <article className='page-content-container'>
+        <Header />
+        <section className='clients-container'>
+          <div className='page-title-container'>
+            <span className='page-title'>Dashboard</span>
+          </div>
+          
+        </section>
+        <section style={{width: '100wv'}}>
+          <p>¡Proximamente!</p>
+        </section>
+      </article>
+      
+      {showModal && <PopupSuppliers closeModal={setShowModal} />}
+
+    </main>
+  );
 }
 
-export default Dashboard;
+export default Dashboard
