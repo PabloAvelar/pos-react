c3fcec7428d3d67e641e659793e87597c5d4d329
PabloAvelar <pabloarmenta84@gmail.com>
2024-04-15 02:20:52 -0600

    fix client service API and datatable

--- src/services/clientsService.js
+++ src/services/clientsService.js
@@ -1,6 +1,6 @@
 import { postByUrl, getByUrl, postByUrlWithConfig } from "./utils";
 
-const baseUrl = 'http://localhost/pos-backend/api/controllers';
+const baseUrl = 'http://localhost/pos-backend/api/clients';
 
 // PUT pero en realidad es post porque pinche php es una mierda
 async function putClient(data){
